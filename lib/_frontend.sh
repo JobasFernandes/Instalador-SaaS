a2f07dd60e56b8c1e4357553a5532c90992923cc
JSFernandes <joseph.automatus@gmail.com>
2023-04-19 16:27:37 -0300

    Ajustes no server.js

--- lib/_frontend.sh
+++ lib/_frontend.sh
@@ -110,11 +110,15 @@ sudo su - deploy << EOF
 const express = require("express");
 const path = require("path");
 const app = express();
+require('dotenv').config();
+
 app.use(express.static(path.join(__dirname, "build")));
-app.get("/*", function (req, res) {
+app.get("/*", function (_req, res) {
 	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+
 app.listen(${frontend_port});
+
 
 [-]EOF
 EOF
